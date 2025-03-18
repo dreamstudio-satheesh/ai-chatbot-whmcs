53d1430c874ec03526587b10ea7b078e73d48f2d
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-03-19 03:07:32 +0530

    app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from routes import tickets, chatbot
 
-router = FastAPI(title="AI Chatbot & Ticket Answering System")
+app = FastAPI(title="AI Chatbot & Ticket Answering System")
 
 # Include API Routes
-router.include_router(tickets.router, prefix="/api")
-router.include_router(chatbot.router) 
+app.include_router(tickets.router, prefix="/api")
+app.include_router(chatbot.router) 
 
 
 
-@router.get("/")
+@app.get("/")
 async def root():
     return {"message": "AI Chatbot API is running!"}
