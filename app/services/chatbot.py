ced99dea8486adc37c889bafe27a6613535e1ca4
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-03-18 11:25:13 +0530

    app

--- app/services/chatbot.py
+++ app/services/chatbot.py
@@ -5,7 +5,7 @@ openai.api_key = os.getenv("OPENROUTER_API_KEY")
 
 def chat_with_ai(user_input: str):
     response = openai.ChatCompletion.create(
-        model="gpt-4-turbo",
+        model="google/gemma-3-1b-it:free",
         messages=[{"role": "user", "content": user_input}]
     )
     return response["choices"][0]["message"]["content"]
